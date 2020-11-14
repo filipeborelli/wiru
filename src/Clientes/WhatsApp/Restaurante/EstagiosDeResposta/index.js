0f5ab24eb6a392493cc60a02954432169fe5997f
Filipe Borelli <60223308+filipeborelli@users.noreply.github.com>
2020-11-14 18:24:52 -0300

    Update index.js
    Remake requires.

--- src/Clientes/WhatsApp/Restaurante/EstagiosDeResposta/index.js
+++ src/Clientes/WhatsApp/Restaurante/EstagiosDeResposta/index.js
@@ -1,39 +1,39 @@
 let stages = {
     0: {
         descricao: "Boas Vindas",
-        obj: require("./Estagios/BemVindo.js"),
+        obj: require("./Estagios/0BemVindo.js"),
     },
     1: {
         descricao: "Pega o nome do cliente",
-        obj: require("./Estagios/NomeCliente.js"),
+        obj: require("./Estagios/1NomeCliente.js"),
     },
     2: {
         descricao: "Envia o Cardápio",
-        obj: require("./Estagios/EnviaCardapio.js"),
+        obj: require("./Estagios/2EnviaCardapio.js"),
     },
     3: {
         descricao: "Carrinho",
-        obj: require("./Estagios/Carrinho.js"),
+        obj: require("./Estagios/3Carrinho.js"),
     },
     4: {
         descricao: "Observação",
-        obj: require("./Estagios/Observacao.js"),
+        obj: require("./Estagios/4Observacao.js"),
     },
     5: {
         descricao: "Pergunta se é para entrega",
-        obj: require("./Estagios/Entrega.js"),
+        obj: require("./Estagios/5Entrega.js"),
     },
     6: {
         descricao: "Pergunta o endereço",
-        obj: require("./Estagios/Endereco.js"),
+        obj: require("./Estagios/6Endereco.js"),
     },
     7: {
         descricao: "Forma de pagamento",
-        obj: require("./Estagios/FormaDePagamento.js"),
+        obj: require("./Estagios/7FormaDePagamento.js"),
     },
     8: {
         descricao: "Encerramento",
-        obj: require("./Estagios/Encerramento.js"),
+        obj: require("./Estagios/8Encerramento.js"),
     },
 
 };
