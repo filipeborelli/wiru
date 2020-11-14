8f4f7edd6a82a54902fe0f3cbb7a765b5d63f287
Filipe Borelli <60223308+filipeborelli@users.noreply.github.com>
2020-11-14 18:30:24 -0300

    Update and rename FormaDePagamento.js to 7FormaDePagamento.js
    Export variables to database.

--- src/Clientes/WhatsApp/Restaurante/EstagiosDeResposta/Estagios/7FormaDePagamento.js
+++ src/Clientes/WhatsApp/Restaurante/EstagiosDeResposta/Estagios/7FormaDePagamento.js
@@ -1,19 +1,20 @@
-const cardapio = require("../../Cardapio/cardapio");
 const banco = require("../../Banco/banco");
 
-function execute(user, msg, contato) {
-
+function execute(user, msg, NomeDoCliente, Carrinho, Observacao, Entrega) {
+    let Endereco = msg;
+    banco.db[user].endereco = Endereco;
     banco.db[user].stage = 8;
     let resumo = "  RESUMO DO PEDIDO \n";
+    let pedidos = "";
     let total = 0;
     banco.db[user].itens.forEach((value) => {
-        console.log(value);
         resumo += `${value.produto} - R$${value.preco} \n`;
-
+        pedidos += `${value.produto} /`
         total += parseFloat(value.preco);
     });
     resumo += "-------------------------\n";
     resumo += ` Total R$ ${total}`;
+    banco.db[user].pedido = pedidos;
     return ["Para confirmar digite # ou para cancelar digite *", resumo];
 }
 
