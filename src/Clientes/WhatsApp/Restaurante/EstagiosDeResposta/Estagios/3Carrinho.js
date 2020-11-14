2e69a417ddae577c045444d8316db8b2ee60f18a
Filipe Borelli <60223308+filipeborelli@users.noreply.github.com>
2020-11-14 18:27:52 -0300

    Update and rename Carrinho.js to 3Carrinho.js
    Rename file to 3Carrinho.js, and export variables to database.

--- src/Clientes/WhatsApp/Restaurante/EstagiosDeResposta/Estagios/3Carrinho.js
+++ src/Clientes/WhatsApp/Restaurante/EstagiosDeResposta/Estagios/3Carrinho.js
@@ -1,29 +1,32 @@
 const cardapio = require("../../Cardapio/cardapio");
 const banco = require("../../Banco/banco");
+const nome = require('./2EnviaCardapio');
 
-function execute(user, msg) {
-    if (msg.toUpperCase() == "CANCELAR") {
+function execute(user, msg, NomeDoCliente, Observacao, Entrega, Endereco) {
+    let Carrinho = msg;
+    module.exports = Carrinho;
+    if (Carrinho.toUpperCase() == "CANCELAR") {
         banco.db[user].stage = 0;
         return ["Pedido cancelado com sucesso"];
     }
 
-    if (msg.toUpperCase() == "FINALIZAR") {
+    if (Carrinho.toUpperCase() == "FINALIZAR") {
         banco.db[user].stage = 4;
         return ["Estamos fechando seu pedido, ok?"];
     }
 
-    if (!cardapio.menu[msg.toUpperCase()]) {
+    if (!cardapio.menu[Carrinho.toUpperCase()]) {
         return [
             "Digite *'FINALIZAR'* para finalizar ou *'CANCELAR'* para cancelar",
             "Código inválido, digite corretamente",
         ];
     }
 
-    banco.db[user].itens.push(cardapio.menu[msg.toUpperCase()]);
+    banco.db[user].itens.push(cardapio.menu[Carrinho.toUpperCase()]);
     return [
         "Digite *'FINALIZAR'* para finalizar os pedidos",
-        `Item(${cardapio.menu[msg].produto}) adiconado com sucesso`,
+        `Item(${cardapio.menu[Carrinho].produto}) adiconado com sucesso`,
     ];
-}
 
+}
 exports.execute = execute;
